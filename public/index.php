fe8638437102ca5a5e2d78c2eeea747dd8d1b5f8
Romain Doukhan <romaindoukhan@gmail.com>
2021-10-31 18:09:58 +0100

    solution

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '/Users/romaindoukhan/Documents/Dev_web/Code/PHP/Composer_2/App/Wcs/Hello.php';
+require_once '/Users/romaindoukhan/Documents/Dev_web/Code/PHP/Composer_2/vendor/autoload.php';
+
+$hello = new Hello();
+echo $hello->talk();
